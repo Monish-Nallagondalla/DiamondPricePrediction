ff28a5a60b634af2be7b8dce1194b36a3c453856
Monish Nallagondalla <nsmonish@gmail.com>
2024-11-18 02:01:46 +0530

    data ingestion file complete

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -31,8 +31,20 @@ class DataIngestion:
             logging.info('Dataset read as pandas Dataframe')
 
             os.makedirs(os.path.join(self.ingestion_config.raw_data_path),exist_ok=True)
-            df.to_csv(self.ingestion_config.raw_data_path)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False)
             logging.info('Raw data is created')
+
+            train_set,test_set = train_test_split(df,test_size=0.30,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info('Ingestion of Data complete')
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
 
         except Exception as e:
             logging.info('Exception occured at Data Ingestion stage')
