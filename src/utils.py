546392ee3e98cfc975342a1540ddc5576270315c
Monish Nallagondalla <nsmonish@gmail.com>
2024-11-20 23:09:30 +0530

    Training pipeline done

--- src/utils.py
+++ src/utils.py
@@ -36,7 +36,7 @@ def evaluate_model (X_train,y_train,X_test,y_test,models):
             y_test_pred = model.predict(X_test)
 
             test_model_score = r2_score(y_test,y_test_pred)
-            report [list(models.key())[i]] = test_model_score
+            report [list(models.keys())[i]] = test_model_score
 
         return report 
     
