626471629bb7d64631911c9f19bd90c72215334e
Monish Nallagondalla <nsmonish@gmail.com>
2024-11-20 23:00:23 +0530

    model trainer completed

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -8,6 +8,7 @@ from src.exception import CustomException
 from src.logger import logging
 
 from src.utils import save_object
+from src.utils import evaluate_model
 from dataclasses import dataclass
 import os,sys
 
@@ -27,7 +28,7 @@ class ModelTrainer:
                 train_array[:,-1],
                 test_array[:,:-1],
                 test_array[:,-1]
-            )
+                )
 
             models ={
                 'LinearRegression':LinearRegression(),
@@ -36,10 +37,35 @@ class ModelTrainer:
                 'Elasticnet':ElasticNet(),
                 'DecisionTree': DecisionTreeRegressor(),
                 'RandomForest' : RandomForestRegressor(),
-                
+
 
 
             }
+            model_report:dict = evaluate_model(X_train,y_train,X_test,y_test,models)
+            print ('\n===================================================================================\n')
+            logging.info(f'Model Report:{model_report}')
+
+
+            # to get the best model 
+
+            best_model_score = max(sorted(model_report.values()))
+
+            best_model_name = list (model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
+
+            print(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
+            print('\n====================================================================================\n')
+            logging.info(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+
+
 
 
         except Exception as e:
