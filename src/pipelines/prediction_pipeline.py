2abdfd4acedaf357963ab88eaf0e7aa50a1fddd3
Monish Nallagondalla <nsmonish@gmail.com>
2024-11-21 23:46:53 +0530

    prediction pipeline completed

--- src/pipelines/prediction_pipeline.py
+++ src/pipelines/prediction_pipeline.py
@@ -24,3 +24,53 @@ class PredictPipeline:
             pred = model.predict(data_scaled)
 
             return pred
+        
+
+        except Exception as e:
+            logging.info('Exception occurred in prediction')
+            raise CustomException(e,sys)
+        
+
+class CustomData:
+    def __init__(self,
+                 carat:float,
+                 depth:float,
+                 table:float,
+                 x:float,
+                 y:float,
+                 z:float,
+                 cut:str,
+                 color:str,
+                 clarity:str):
+        
+        self.carat = carat
+        self.depth = depth
+        self.table = table
+        self.x = x
+        self.y = y
+        self.z = z
+        self.cut = cut 
+        self.color = color
+        self.clarity = clarity
+
+    def get_data_as_dataframe(self):
+        try:
+            custom_data_input_dict = {
+                'carat':[self.carat],
+                'depth':[self.depth],
+                'table':[self.table],
+                'x':[self.x],
+                'y':[self.y],
+                'z':[self.z],
+                'cut':[self.cut],
+                'color':[self.color],
+                'clarity':[self.clarity]
+            }
+
+            df = pd.DataFrame(custom_data_input_dict)
+            logging.info('Dataframe Gathered')
+            return df 
+        
+        except Exception as e:
+            logging.info('Exception occured in prediction pipeline')
+            raise CustomException(e,sys)
