60be94da30fbea4cc7c5855b271785644880ab50
Monish Nallagondalla <nsmonish@gmail.com>
2024-11-23 23:48:34 +0530

    application starts

--- application.py
+++ application.py
@@ -1,1 +1,6 @@
-from flask import Flask, request,render
+from flask import Flask, request,render_template,jasonify
+from src.pipelines.prediction_pipeline import CustomData,PredictPipeline
+
+application = Flask(__name__)
+
+app = application
