8ff338eb764d3f82e503b1791723a798204aae65
Monish Nallagondalla <nsmonish@gmail.com>
2024-12-03 23:27:25 +0530

    complettttteeeeee

--- application.py
+++ application.py
@@ -1,13 +1,12 @@
-from flask import Flask, request,render_template,jasonify
+from flask import Flask,request,render_template,jsonify
 from src.pipelines.prediction_pipeline import CustomData,PredictPipeline
 
-application = Flask(__name__)
 
-app = application
+application=Flask(__name__)
 
+app=application
 
 @app.route('/')
-
 def home_page():
     return render_template('index.html')
 
@@ -16,11 +15,11 @@ def home_page():
 def predict_datapoint():
     if request.method=='GET':
         return render_template('form.html')
-
+    
     else:
-        data = CustomData(
-            carat = float(request.form.get('carat')),
-            depth= float(request.form.get('depth')),
+        data=CustomData(
+            carat=float(request.form.get('carat')),
+            depth = float(request.form.get('depth')),
             table = float(request.form.get('table')),
             x = float(request.form.get('x')),
             y = float(request.form.get('y')),
@@ -29,3 +28,14 @@ def predict_datapoint():
             color= request.form.get('color'),
             clarity = request.form.get('clarity')
         )
+        final_new_data=data.get_data_as_dataframe()
+        predict_pipeline=PredictPipeline()
+        pred=predict_pipeline.predict(final_new_data)
+
+        results=round(pred[0],2)
+
+        return render_template('form.html',final_result=results)
+    
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0',debug=True)
