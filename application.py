1ca1d7c5710e1291e10418a4bc92f50db5abc2f7
Monish Nallagondalla <nsmonish@gmail.com>
2024-12-03 23:09:47 +0530

    adding changes

--- application.py
+++ application.py
@@ -4,3 +4,20 @@ from src.pipelines.prediction_pipeline import CustomData,PredictPipeline
 application = Flask(__name__)
 
 app = application
+
+
+@app.route('/')
+
+def home_page():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['GET','POST'])
+
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('form.html')
+
+    else:
+        data = CustomData(
+            
+        )
